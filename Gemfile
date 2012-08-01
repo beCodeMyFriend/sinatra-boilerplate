b247aecbf8dd9390605253b7f8d413bc8674fc93
Miguel Angel <mike@becodemyfriend.com>
2012-08-01 15:35:00 +0200

    added json dependency

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 gem "sinatra"
+gem "json"
 
 #bundle install --without test development
 group :test do
--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra/base'
+require 'json'
 
 class MyApp < Sinatra::Base
 
   get '/' do
-    "Hello World"
+    {:say => "Hello World"}.to_json
   end
 
 end
